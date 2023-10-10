1acc8f1becc86eb2e5061625b38374dbd1694c89
ceciliaknaub <ccknaub@gmail.com>
2023-10-10 18:51:57 -0400

    Svg, text updates, remove h1 tags

--- scripts/main.js
+++ scripts/main.js
@@ -5,11 +5,10 @@ console.log(link);
 
 link.addEventListener("mouseover", function() {
     let show = document.querySelector(".link-box");
-    show.style.opacity = "1"
+    show.style.opacity = "0.75"
 });
 
 link.addEventListener("mouseout", function() {
     let show = document.querySelector(".link-box");
     show.style.opacity = "0"
 });
-
