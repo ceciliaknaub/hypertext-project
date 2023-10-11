63fc8d61f7eaa32a0b612f62334242f606fd64f3
ceciliaknaub <ccknaub@gmail.com>
2023-10-11 18:12:05 -0400

    remove link box svg and js, hover text

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,1 @@
-let link = document.querySelector('.index-link');
-console.log(link);
-
-link.addEventListener("mouseover", function() {
-    let show = document.querySelector(".link-box");
-    show.style.opacity = "0.75"
-});
-
-link.addEventListener("mouseout", function() {
-    let show = document.querySelector(".link-box");
-    show.style.opacity = "0"
-});
+l
